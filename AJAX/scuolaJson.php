22aee5bc0588f76a621a429cc69279c519b1bc19
angelogalanti <angelo.galanti@gmail.com>
2019-02-14 08:48:59 +0100

    Add files via upload

--- AJAX/scuolaJson.php
+++ AJAX/scuolaJson.php
@@ -1,21 +1,26 @@
 <?php
 
+$persona1 = new stdClass();
 $persona1->name = "A";
 $persona1->age = 10;
 $persona1->city = "New York";
 
+$persona2 = new stdClass();
 $persona2->name = "B";
 $persona2->age = 20;
 $persona2->city = "New";
 
+$persona3 = new stdClass();
 $persona3->name = "C";
 $persona3->age = 30;
 $persona3->city = "York";
 
+$persona4 = new stdClass();
 $persona4->name = "D";
 $persona4->age = 40;
 $persona4->city = "NewYork";
 
+$persona5 = new stdClass();
 $persona5->name = "E";
 $persona5->age = 50;
 $persona5->city = "Nk";
